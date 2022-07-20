07ebe110f010153877486c53e3f52cf9d639fae8
ram <chakradhar.thatikonda933@gmail.com>
2022-07-20 11:33:41 +0530

    first commit

---
+++ route53/route.tf
@@ -0,0 +1,35 @@
+resource "aws_route_table" "rt1" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+ tags = {
+    Name = "Custom"
+  }
+}
+
+resource "aws_route_table" "rt2" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.ngw.id
+  }
+
+ tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_route_table_association" "as_1" {
+  subnet_id      = aws_subnet.Pub.id
+  route_table_id = aws_route_table.rt1.id
+}
+
+resource "aws_route_table_association" "as_2" {
+  subnet_id      = aws_subnet.pri.id
+  route_table_id = aws_route_table.rt2.id
+}
